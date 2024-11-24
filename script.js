4554530d90b9d67083db22ea16e6b389c83b69d2
milo2828 <sweetsextyn@gmail.com>
2024-11-24 15:32:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    document.querySelector('.nav-links').classList.toggle('active');
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('open');
 });
